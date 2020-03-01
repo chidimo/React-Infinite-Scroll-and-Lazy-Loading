0a13a0021ee026cd9fbc561c9ea3d21d38e64202
Chidi Orji <orjichidi95@gmail.com>
2020-03-01 23:47:04 +0100

    chore: setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="/#">
+            <h2>Infinite scroll + image lazy loading</h2>
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
