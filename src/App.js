11fe19634b2a8c099858d3e02076ed41db891771
Chidi Orji <orjichidi95@gmail.com>
2020-03-02 02:11:20 +0100

    feat: make api call to show images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 import './index.css';
 
 function App() {
+  const imgReducer = (state, action) => {
+    switch (action.type) {
+      case 'STACK_IMAGES':
+        return { ...state, images: state.images.concat(action.images) }
+      case 'FETCHING_IMAGES':
+        return { ...state, fetching: action.fetching }
+      default:
+        return state;
+    }
+  }
+
+  const [imgData, imgDispatch] = useReducer(imgReducer, { images: [], fetching: true, })
+
+  // make API calls
+  useEffect(() => {
+    imgDispatch({ type: 'FETCHING_IMAGES', fetching: true })
+    fetch('https://picsum.photos/v2/list?page=0&limit=10')
+      .then(data => data.json())
+      .then(images => {
+        imgDispatch({ type: 'STACK_IMAGES', images })
+        imgDispatch({ type: 'FETCHING_IMAGES', fetching: true })
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  }, [ imgDispatch ])
+
   return (
-    <div className="App">
+    <div className="">
       <nav className="navbar bg-light">
         <div className="container">
           <a className="navbar-brand" href="/#">
@@ -12,6 +39,28 @@ function App() {
           </a>
         </div>
       </nav>
+
+      <div id='images' className="container">
+        <div className="row">
+          {imgData.images.map((image, index) => {
+            const { author, download_url } = image
+            return (
+              <div key={index} className="card">
+                <div className="card-body ">
+                  <img
+                    alt={author}
+                    className="card-img-top"
+                    src={download_url}
+                  />
+                </div>
+                <div className="card-footer">
+                  <p className="card-text text-center text-capitalize text-primary">Shot by: {author}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
