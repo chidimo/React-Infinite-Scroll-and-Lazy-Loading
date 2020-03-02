a689839d0a1757fe8d68e966a0526c7e5d0f20ac
Chidi Orji <orjichidi95@gmail.com>
2020-03-02 18:42:38 +0100

    refactor: use custom hooks

---
+++ src/customHooks.js
@@ -0,0 +1,70 @@
+import { useEffect, useCallback, useRef } from 'react';
+
+// make API calls and pass the returned data via dispatch
+export const useFetch = (data, dispatch) => {
+  useEffect(() => {
+    dispatch({ type: 'FETCHING_IMAGES', fetching: true })
+    fetch(`https://picsum.photos/v2/list?page=${data.page}&limit=10`)
+      .then(data => data.json())
+      .then(images => {
+        dispatch({ type: 'STACK_IMAGES', images })
+        dispatch({ type: 'FETCHING_IMAGES', fetching: true })
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  }, [dispatch, data.page])
+}
+
+// infinite scrolling with intersection observer
+export const useInfiniteScroll = (scrollRef, dispatch) => {
+  const scrollObserver = useCallback(
+    node => {
+      new IntersectionObserver(entries => {
+        entries.forEach(en => {
+          if (en.intersectionRatio > 0) {
+            dispatch({ type: 'ADVANCE_PAGE' });
+          }
+        });
+      }).observe(node);
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollObserver(scrollRef.current);
+    }
+  }, [scrollObserver, scrollRef]);
+}
+
+// lazy load images with intersection observer
+export const useLazyLoading = (imgSelector, items) => {
+  const imgObserver = useCallback(node => {
+    new IntersectionObserver(entries => {
+      entries.forEach(en => {
+        if (en.intersectionRatio > 0) {
+          const currentImg = en.target;
+          const newImgSrc = currentImg.dataset.src;
+
+          // only swap out the image source if the new url exists
+          if (!newImgSrc) {
+            console.error('Image source is invalid');
+          } else {
+            currentImg.src = newImgSrc;
+          }
+        }
+      });
+    }).observe(node);
+  }, []);
+
+  const imagesRef = useRef(null);
+
+  useEffect(() => {
+    imagesRef.current = document.querySelectorAll(imgSelector);
+
+    if (imagesRef.current) {
+      imagesRef.current.forEach(img => imgObserver(img));
+    }
+  }, [imgObserver, imagesRef, imgSelector, items])
+}
